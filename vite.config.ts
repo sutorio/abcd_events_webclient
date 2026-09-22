2780e9f37ca29f31c83c5b4195c4b645f54bbcb3
DanCouper <danielcouper.sutor@gmail.com>
2026-09-22 21:00:49 +0100

    Tighten up configs and text editor settings. Update Vite configuration to import dependencies directly, removing anything that isn't available at runtime from the import mapin deno.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "npm:vite@^8.3.0";
+import { default as solidPlugin } from "npm:@solidjs/vite-plugin@^3.0.0-next.44";
+import { fileRoutes } from "npm:filesystem-routing@0.3.0/vite";
+import "npm:@solidjs/diagnostics@^2.0.0-rc.9";
+
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  clearScreen: false,
+  plugins: [
+    solidPlugin({
+      start: true,
+      extensions: [".jsx", ".tsx"],
+      diagnostics: true,
+    }),
+    fileRoutes(),
+  ],
+});
